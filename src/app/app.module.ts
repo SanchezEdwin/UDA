d55d2b42ed3228e137ae02cefa2e47c515ed91d3
EdwinS <sanchezescandonedwin@gmail.com>
2023-04-16 04:00:27 -0500

    Actividad de vacaciones

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Page2Component } from './page2/page2.component';
+import { Page3Component } from './page3/page3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Page2Component,
+    Page3Component
   ],
   imports: [
     BrowserModule,
