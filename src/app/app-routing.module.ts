194c216f5107b81c072358b2d2d6d596e05448e3
EdwinS <sanchezescandonedwin@gmail.com>
2023-04-23 04:00:30 -0500

    VFinal

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Page2Component } from './page2/page2.component';
 import { Page3Component } from './page3/page3.component';
+import { Page4Component } from './page4/page4.component';
+import { Page5Component } from './page5/page5.component';
 
 const routes: Routes = [
   {path:'page2', component:Page2Component},
-  {path:'page3', component:Page3Component}
+  {path:'page3', component:Page3Component},
+  {path: 'page4', component:Page4Component},
+  {path: 'page5', component:Page5Component}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
 import { Page3Component } from './page3/page3.component';
+import { Page4Component } from './page4/page4.component';
+import { Page5Component } from './page5/page5.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
+    Page1Component,
     Page2Component,
-    Page3Component
+    Page3Component,
+    Page4Component,
+    Page5Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot([
+      {path: '', component:Page1Component},
+      {path: 'page2', component:Page2Component},
+      {path: 'page3', component:Page3Component},
+      {path: 'page4', component:Page4Component},
+      {path: 'page5', component:Page5Component},
+      {path: '**', redirectTo:'/', pathMatch: 'full'},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
